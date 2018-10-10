b7e99c64504427ce9f1bfdcf0329bf23194d599f
ibule <taotaohu3.14@qq.com>
2018-10-10 13:31:06 +0800

    ok

--- src/main.js
+++ src/main.js
@@ -1,8 +1,33 @@
 import Vue from 'vue'
 import App from './App.vue'
+//导入路由相关
+import VueRouter from "vue-router";
+//导入首页组件
+import index from "./components/index.vue";
+Vue.use(VueRouter);
+//导入轮播图插件
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false
+//写规则
+const routes=[
+  {
+    path:"/",
+    component:index
 
+  },
+  {
+    path:"/index",
+    component:index
+  }
+]
+//实例化路由对象
+const router=new VueRouter({
+  routes
+})
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router
 }).$mount('#app')
